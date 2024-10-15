ddf122d7b1a6172981497bca7f4fc7876e95076c
promise1402 <promise1509@gmail.com>
2024-10-15 17:14:49 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Game from './Game.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
